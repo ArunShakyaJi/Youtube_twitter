948664ccd3db4cefc8a1646b389fffed3c3c9611
ArunShakyaJi <shakyaarun173@gmail.com>
2024-05-10 19:43:37 +0530

    chore: Update multer middleware to create upload directory if it doesn't exist

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,14 +1,20 @@
 import multer from "multer";
+import fs from "fs";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+        const uploadDir = "./public";
+        // Check if the directory exists, if not, create it
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir);
+        }
+        cb(null, uploadDir);
     },
     filename: function (req, file, cb) {
-      cb(null, Date.now() + file.originalname)
+        cb(null, Date.now() + file.originalname);
     }
-  })
-  
+});
+
 export const upload = multer({ 
     storage,
-})
+});
